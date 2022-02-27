f874dc1709b80e7f0340aa1bd14043e9c5a9256f
Romit Shrestha <romit.shrestha72@gmail.com>
2022-02-27 21:05:17 +0545

    second

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-console.log("Project Setup")
-=======
 console.log("this is beginning of app");
->>>>>>> 02cee8c0f916338343d2b4f3e2b492b88c39d84e
+a=() =>{
+    console.log("hey")
+}
+a()
