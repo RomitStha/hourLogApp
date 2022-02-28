fc453052e87f803cfa9cba19fc3ce8a724fe4fb0
Romit Shrestha <romit.shrestha72@gmail.com>
2022-02-28 10:59:02 +0545

    hy

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-console.log("this is beginning of app");
-let a=() =>{
-    console.log("hey")
-}
-a()
-
-<<<<<<< HEAD
-
-
-
-let b = () =>{
-    console.log("b")
-}
-b()
-=======
-console.log("we can take the both as well")
->>>>>>> 373d84668ca70eb9e753b4b162ccd2ba2f78c7d5
+console.log("hy")
